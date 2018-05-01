8e5e83fbf2778ea8e167a2305aabc02ee4d6381c
mkartik202001 <31443607+mkartik202001@users.noreply.github.com>
2018-05-01 09:33:48 -0500

    adfa

---
+++ CollageRunner.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Write a description of class Collage here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CollageRunner 
+{
+    Picture picture1 = new Picture ("images\\BatmanOriginal.jpeg"); 
+    p
+   
+
+}
--- Picture.java
+++ Picture.java
@@ -1,40 +1,123 @@
-
+// Kartik Maheshwari
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.text.*;
+import java.util.*;
+import java.util.List; // resolves problem with java.awt.List and java.util.List
 
 /**
- * Write a description of class Picture here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Original Picture Size : 748 X 421
+ * Canvas size should be: 2244 X 842
  */
-public class Picture
+
+/**
+ * A class that represents a picture.  This class inherits from
+ * SimplePicture and allows the student to add functionality to
+ * the Picture class.
+ *
+ * Copyright Georgia Institute of Technology 2004-2005
+ * @author Barbara Ericson ericson@cc.gatech.edu
+ */
+public class Picture extends CollageRunner
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
+  ///////////////////// constructors //////////////////////////////////
 
-    /**
-     * Default constructor for objects of class Picture
+  /**
+   * Constructor that takes no arguments
+   */
+  public void TestPicture17 ()
+  {
+    /* not needed but use it to show students the implicit call to super()
+     * child constructors always call a parent constructor
      */
-    public Picture()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+    super();
+  }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
-    }
+  /**
+   * Constructor that takes a file name and creates the picture
+   * @param fileName the name of the file to create the picture from
+   */
+  public Picture(String fileName)
+  {
+    // let the parent class handle this fileName
+    super(fileName);
+  }
 
+  /**
+   * Constructor that takes the width and height
+   * @param width the width of the desired picture
+   * @param height the height of the desired picture
+   */
+  public Picture(int width, int height)
+  {
+    // let the parent class handle this width and height
+    super(width,height);
+  }
+
+  /**
+   * Constructor that takes a picture and creates a
+   * copy of that picture
+   */
+  public Picture(Picture copyPicture)
+  {
+    // let the parent class do the copy
+    super(copyPicture);
+  }
+
+  /**
+   * Constructor that takes a buffered image
+   * @param image the buffered image to use
+   */
+  public Picture(BufferedImage image)
+  {
+    super(image);
+  }
+
+  ////////////////////// methods ///////////////////////////////////////
+
+  /**
+   * Method to return a string with information about this picture.
+   * @return a string with information about the picture such as fileName,
+   * height and width.
+   */
+  public String toString()
+  {
+    String output = "Picture, filename " + getFileName() +
+      " height " + getHeight()
+      + " width " + getWidth();
+    return output;
+
+  }
+
+  public static void main(String[] args)
+  {
+     String fileName = FileChooser.pickAFile();
+     Picture pictObj = new Picture(fileName);
+     pictObj.explore();
+  }
+
+  // --------------------My Methords-------------------------
+
+  public void gray()
+  {
+	  int average =0;
+	  Pixel[] pixelArray = this.getPixels(); // this copies pixels in the array.
+	  for (Pixel spot : pixelArray)
+	  {
+		  int redValue = spot.getRed();
+	      int greenValue = spot.getGreen();
+	      int blueValue = spot.getBlue();
+	      average = (redValue + greenValue + blueValue) / 3;
+
+	      spot.setRed(average);
+	      spot.setGreen(average);
+	      spot.setBlue(average);
+	   }
+	   this.explore();
+
+  }
+  
+  public void recursive(
 }
